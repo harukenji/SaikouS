cafe93ce4eadece11378f979204183eb37b8560e
LawlietAnime <ryusakilawlietgithub@gmail.com>
2023-02-04 15:20:05 +0100

    Initial Commit

---
+++ Saikou Beta/Saikou_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saikou_BetaApp.swift
+//  Saikou Beta
+//
+//  Created by Inumaki on 04.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Saikou_BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
